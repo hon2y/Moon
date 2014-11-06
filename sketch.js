57cf321114bb4d706f85c0159cc40c7823391e49
hon2y <hon222y@gmail.com>
2014-11-06 16:06:57 +0900

    d

--- sketch.js
+++ sketch.js
@@ -18,8 +18,8 @@ function draw() {
    var timing = (new Date()%duration)/duration;
 
    fill(100,0,0);
-   ellipse(600 + Math.cos(timing*2*PI)*200,           // x좌표
-        600 + Math.sin(timing*2*PI)*200,       // y좌표
+   ellipse(600 + Math.cos(timing*2*PI)*100,           // x좌표
+        600 + Math.sin(timing*2*PI)*100,       // y좌표
         10,                                     // width
         10);                                    // height
 
@@ -28,8 +28,8 @@ function draw() {
    var timing = (new Date()%duration)/duration;
 
    fill(100,50,0);
-   ellipse(600 + Math.cos(timing*2*PI)*250,           // x좌표
-        600 + Math.sin(timing*2*PI)*250,       // y좌표
+   ellipse(600 + Math.cos(timing*2*PI)*150,           // x좌표
+        600 + Math.sin(timing*2*PI)*150,       // y좌표
         30,                                     // width
         30);                                    // height
 
@@ -38,8 +38,8 @@ function draw() {
    var timing = (new Date()%duration)/duration;
 
    fill(100,50,0);
-   ellipse(600 + Math.cos(timing*2*PI)*300,           // x좌표
-        600 + Math.sin(timing*2*PI)*300,       // y좌표
+   ellipse(600 + Math.cos(timing*2*PI)*200,           // x좌표
+        600 + Math.sin(timing*2*PI)*200,       // y좌표
         40,                                     // width
         40);                                    // height
 
@@ -48,8 +48,8 @@ function draw() {
    var timing = (new Date()%duration)/duration;
 
    fill(100,50,0);
-   ellipse(600 + Math.cos(timing*2*PI)*300,           // x좌표
-        600 + Math.sin(timing*2*PI)*300,       // y좌표
+   ellipse(600 + Math.cos(timing*2*PI)*250,           // x좌표
+        600 + Math.sin(timing*2*PI)*250,       // y좌표
         20,                                     // width
         20);                                    // height
 
@@ -68,8 +68,8 @@ function draw() {
    var timing = (new Date()%duration)/duration;
 
    fill(100,50,0);
-   ellipse(600 + Math.cos(timing*2*PI)*400,           // x좌표
-        600 + Math.sin(timing*2*PI)*400,       // y좌표
+   ellipse(600 + Math.cos(timing*2*PI)*450,           // x좌표
+        600 + Math.sin(timing*2*PI)*450,       // y좌표
         100,                                     // width
         100);                                    // height
 
@@ -78,8 +78,8 @@ function draw() {
    var timing = (new Date()%duration)/duration;
 
    fill(100,50,0);
-   ellipse(600 + Math.cos(timing*2*PI)*450,           // x좌표
-        600 + Math.sin(timing*2*PI)*450,       // y좌표
+   ellipse(600 + Math.cos(timing*2*PI)*550,           // x좌표
+        600 + Math.sin(timing*2*PI)*550,       // y좌표
         70,                                     // width
         70);                                    // height
 
@@ -88,8 +88,8 @@ function draw() {
    var timing = (new Date()%duration)/duration;
 
    fill(100,50,0);
-   ellipse(600 + Math.cos(timing*2*PI)*550,           // x좌표
-        600 + Math.sin(timing*2*PI)*550,       // y좌표
+   ellipse(600 + Math.cos(timing*2*PI)*600,           // x좌표
+        600 + Math.sin(timing*2*PI)*600,       // y좌표
         60,                                     // width
         60);                                    // height
 }
