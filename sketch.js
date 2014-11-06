41c5f651bfb59417a7b27c565afcaabb779a157b
hon2y <hon222y@gmail.com>
2014-11-06 16:04:46 +0900

    s

--- sketch.js
+++ sketch.js
@@ -20,8 +20,8 @@ function draw() {
    fill(100,0,0);
    ellipse(600 + Math.cos(timing*2*PI)*200,           // x좌표
         600 + Math.sin(timing*2*PI)*200,       // y좌표
-        20,                                     // width
-        20);                                    // height
+        10,                                     // width
+        10);                                    // height
 
 //금
    var duration = 4000;
@@ -30,21 +30,11 @@ function draw() {
    fill(100,50,0);
    ellipse(600 + Math.cos(timing*2*PI)*250,           // x좌표
         600 + Math.sin(timing*2*PI)*250,       // y좌표
-        50,                                     // width
-        50);                                    // height
+        30,                                     // width
+        30);                                    // height
 
 //지
-   var duration = 4000;
-   var timing = (new Date()%duration)/duration;
-
-   fill(100,50,0);
-   ellipse(600 + Math.cos(timing*2*PI)*300,           // x좌표
-        600 + Math.sin(timing*2*PI)*300,       // y좌표
-        60,                                     // width
-        60);                                    // height
-
-//화
-   var duration = 4000;
+   var duration = 2000;
    var timing = (new Date()%duration)/duration;
 
    fill(100,50,0);
@@ -53,43 +43,53 @@ function draw() {
         40,                                     // width
         40);                                    // height
 
+//화
+   var duration = 5000;
+   var timing = (new Date()%duration)/duration;
+
+   fill(100,50,0);
+   ellipse(600 + Math.cos(timing*2*PI)*300,           // x좌표
+        600 + Math.sin(timing*2*PI)*300,       // y좌표
+        20,                                     // width
+        20);                                    // height
+
 //목
-   var duration = 4000;
+   var duration = 6000;
    var timing = (new Date()%duration)/duration;
 
    fill(100,50,0);
    ellipse(600 + Math.cos(timing*2*PI)*350,           // x좌표
         600 + Math.sin(timing*2*PI)*350,       // y좌표
-        180,                                     // width
-        180);                                    // height
+        120,                                     // width
+        120);                                    // height
 
 //토
-   var duration = 4000;
+   var duration = 7000;
    var timing = (new Date()%duration)/duration;
 
    fill(100,50,0);
    ellipse(600 + Math.cos(timing*2*PI)*400,           // x좌표
         600 + Math.sin(timing*2*PI)*400,       // y좌표
-        160,                                     // width
-        160);                                    // height
+        100,                                     // width
+        100);                                    // height
 
 //천
-   var duration = 4000;
+   var duration = 5000;
    var timing = (new Date()%duration)/duration;
 
    fill(100,50,0);
    ellipse(600 + Math.cos(timing*2*PI)*450,           // x좌표
         600 + Math.sin(timing*2*PI)*450,       // y좌표
-        120,                                     // width
-        120);                                    // height
+        70,                                     // width
+        70);                                    // height
 
 //해
-   var duration = 4000;
+   var duration = 3000;
    var timing = (new Date()%duration)/duration;
 
    fill(100,50,0);
    ellipse(600 + Math.cos(timing*2*PI)*550,           // x좌표
         600 + Math.sin(timing*2*PI)*550,       // y좌표
-        100,                                     // width
-        100);                                    // height
+        60,                                     // width
+        60);                                    // height
 }
